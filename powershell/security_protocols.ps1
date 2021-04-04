ebf32522ce7cc20658fe67f019944ccdf27d5730
David Lorraine <81561437+david-lorraine@users.noreply.github.com>
2021-04-04 13:22:52 +0100

    Update security_protocols.ps1
    Clean up script

--- powershell/security_protocols.ps1
+++ powershell/security_protocols.ps1
@@ -1,16 +1,9 @@
 # Setting the security protocol type in PowerShell for web requests
-
-Clear
-
 # To see the security protocols been currenty used:
 
-Write-Host The current security protocols are: `t -NoNewline
 [Net.ServicePointManager]::SecurityProtocol
 
 # This can be change to: SystemDefault, Ssl3, Tls, Tls11, Tls12 or Tls13
 # Lets set to security protocols to: Tls 1.2 and Tls 1.3
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Tls13
-
-Write-Host `nThe security protocols are now set to:`t -NoNewline
-[Net.ServicePointManager]::SecurityProtocol
